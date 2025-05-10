9ba43e95b7c5e1c7091d4c0821f3ffdf039e09e0
Tushar Cora Suresh <92594865+Corazon0112@users.noreply.github.com>
2025-05-10 09:12:32 -0400

    Add files via upload

---
+++ TestCases/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+   
+    const char* str[] = {"See", "You", "Later", "Alligator"};
+    size_t size = sizeof(str) / sizeof(str[0]);
+
+    for (size_t i = 0; i < size; i++)
+        printf("%s\n", str[i]);
+
+    return EXIT_SUCCESS;
+}
